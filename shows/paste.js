d0666494e37d0f9895c74f274f37b1b641d8f103
Ómar Kjartan Yasin <omarkj@gmail.com>
2011-01-24 23:52:32 +0000

    Got repo in shape, added line highlighter (just add ?line=[1,2,3])

--- shows/paste.js
+++ shows/paste.js
@@ -8,6 +8,18 @@ function(doc, req) {
   var template = t_doc.templates.paste;
   var partials = t_doc.templates.partials;
   
+  var line_highlight = [-1];
+  
+  if ('query' in req) {
+    if ('line' in req.query) {
+      if (Array.isArray(JSON.parse(req.query.line))) {
+        line_highlight = req.query.line;
+      } else {
+        line_highlight = [-1];
+      }
+    }
+  }
+  
   return Mustache.to_html(template,{
     header: {
       PageTitle: doc.title + " ["+doc.language+"]",
@@ -20,6 +32,7 @@ function(doc, req) {
     Created: doc.date,
     hasTags: (doc.tags.length > 0),
     DocPath: Config.DocPath,
+    line_highlight: line_highlight,
     Code: doc.code
   },partials);
 };
